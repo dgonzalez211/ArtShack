f67280f39a9f9e4ef0cb957294ae749e3d2a5269
dmarrugoa <dmarrugoa@uninorte.edu.co>
2021-12-08 17:16:18 -0500

    added mock tests to rest api

--- lib/flutter_flow/flutter_flow_util.dart
+++ lib/flutter_flow/flutter_flow_util.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
